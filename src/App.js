ea42fd21503faa3d1de009e3039bb56da2c22d4e
amitbd599 <amitbd599@gmail.com>
2022-08-09 00:01:03 +0600

    all file add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { Provider } from "react-redux";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import store from "./Redux/Store/store";
+import LazyLoader from "./Components/MasterLayout/Loader";
+import ProductListPage from "./Page/ProductListPage";
+import { faker } from "@faker-js/faker";
 function App() {
+  // const product = [...Array(2000)].map(() => ({
+  //   id: faker.datatype.uuid(),
+  //   name: faker.commerce.product(),
+  //   avatar: faker.image.transport(200, 200, true),
+  //   // avatar: faker.image.food(200, 200, true),
+  //   price: faker.commerce.price(),
+  //   special_price: faker.helpers.arrayElement(["yes", "No"]),
+  //   category: faker.commerce.product(),
+  //   sub_category: faker.commerce.productAdjective(),
+  //   description: faker.commerce.productDescription(),
+  //   companyName: faker.company.companyName(),
+  //   remark: faker.helpers.arrayElement(["NEW", "OLD"]),
+  //   inStock: faker.helpers.arrayElement(["yes", "No"]),
+  //   fastDelivery: faker.datatype.boolean(),
+  //   ratings: faker.helpers.arrayElement([1, 2, 3, 4, 5]),
+  // }));
+
+  // console.log(product);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      {" "}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<ProductListPage />} />
+        </Routes>
+      </BrowserRouter>{" "}
+      <LazyLoader />
+    </Provider>
   );
 }
 
